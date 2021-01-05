fbbd368e4f8159edb4509e4c058fd866a93acf01
scadler <scadler@s.sfusd.edu>
2021-01-05 01:34:29 +0000

    collisions are not currently working

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("plane");
 const context = canvas.getContext("2d");
 bodyA = {
-    m: 3,
+    m: 5,
     xy: [10,50],
     v: 3,
     r: 0,
@@ -14,8 +14,8 @@ bodyB = {
     r: 0,
     theta: 2/5 * Math.PI
 }
-bodyA.r = 10*Math.sqrt(bodyA.m/(2*Math.PI))
-bodyB.r = 10*Math.sqrt(bodyB.m/(2*Math.PI))
+bodyA.r = Math.sqrt(100*bodyA.m/(2*Math.PI))
+bodyB.r = Math.sqrt(100*bodyB.m/(2*Math.PI))
 function drawRect(x, y, w, h, color){
     context.fillStyle = color;
     context.fillRect(x, y, w, h);
@@ -41,14 +41,28 @@ function boundaries(body){
         body.theta = 2*Math.PI - body.theta
     }
 }
-function collision(a,b){
+function collisionDetection(a,b){
     var radius = a.r + b.r;
-    var xCor = a.xy[0] - b.xy[0];
-    var yCor = a.xy[1] - b.xy[1];
+    var xCor = b.xy[0] - a.xy[0];
+    var yCor = b.xy[1] - a.xy[1];
     if(radius > Math.sqrt( ( xCor*xCor ) + (yCor*yCor) )){
         var KE = ( 1/2 * a.m * a.v * a.v ) + ( 1 / 2 * b.m * b.v * b.v )
-        console.log(KE)
+        // console.log(KE+" KE")
+        var phi = Math.atan(xCor/yCor)
+        console.log(phi+" phi")
+        collisionEquation(a,b,phi)
     }
+}
+function collisionEquation(a,b,phi){
+    var vxa =  ( ( a.v * Math.cos(a.theta - phi) * (a.m - b.m) + ( 2 * b.m * b.v * Math.cos(b.theta - phi) ) ) / ( a.m + b.m ) ) * Math.cos(phi) + ( a.v * Math.sin(a.theta - phi) * Math.cos( phi + (Math.PI /2) ) )
+    var vya =  ( ( a.v * Math.cos(a.theta - phi) * (a.m - b.m) + ( 2 * b.m * b.v * Math.cos(b.theta - phi) ) ) / ( a.m + b.m ) ) * Math.sin(phi) + ( a.v * Math.sin(a.theta - phi) * Math.cos( phi + (Math.PI /2) ) )
+    var vxb =  ( ( b.v * Math.cos(b.theta - phi) * (b.m - a.m) + ( 2 * a.m * a.v * Math.cos(a.theta - phi) ) ) / ( b.m + a.m ) ) * Math.cos(phi) + ( b.v * Math.sin(b.theta - phi) * Math.cos( phi + (Math.PI /2) ) )
+    var vyb =  ( ( b.v * Math.cos(b.theta - phi) * (b.m - a.m) + ( 2 * a.m * a.v * Math.cos(a.theta - phi) ) ) / ( b.m + a.m ) ) * Math.sin(phi) + ( b.v * Math.sin(b.theta - phi) * Math.cos( phi + (Math.PI /2) ) )
+    a.v = Math.sqrt((vxa * vxa) + ( vya * vya))
+    a.theta = Math.atan(vxa/vya)
+    console.log(a.v)
+    b.v = Math.sqrt((vxb * vxb) + (vyb * vyb))
+    b.theta = Math.atan(vxb/vyb)
 }
 function placeInCanvas(body){
     if( body.xy[0] + body.r > canvas.width || body.xy[0] < body.r || body.xy[1] + body.r > canvas.height || body.xy[1] < body.r){
@@ -74,7 +88,7 @@ function update(body){
     updatePos(body)
     boundaries(body)
     placeInCanvas(body)
-    collision(bodyA,bodyB)
+    collisionDetection(bodyA,bodyB)
 }
 function system(){
     render();
